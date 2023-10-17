34ff690b7ef42c503601d4b2403e36e6d6b0e033
ilhna-iMac <ilhna@student.42seoul.kr>
2023-10-17 15:39:56 +0900

    [Feat] CheckLeak 함수 추가

--- srcs/util/Error.cpp
+++ srcs/util/Error.cpp
@@ -4,3 +4,9 @@ void Error::Print(const std::string& message)
 {
 	std::cerr << Colors::RedString("Error: " + message) << std::endl;
 }
+
+void Error::CheckLeak(int num)
+{
+	std::cout << "------------" << num << "------------" << std::endl;
+	system("leaks webserv");
+}
